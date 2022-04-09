bc4b4c4174adde4ef68213cabefcc85921505b63
Bohdan Krevsun <bkrevsun@gmail.com>
2022-04-09 13:54:31 +0400

    second commit

--- vacation.js
+++ vacation.js
@@ -188,51 +188,55 @@ let vacationArray = [
   },
 ]
 
-//1.Найти уникальные имена сотрудников в списке
-const uniquesValues = []
+const transform = (initialArray) => {
+  //1.Найти уникальные имена сотрудников в списке
+  const uniquesValues = []
 
-vacationArray.forEach((el) => {
-  if (uniquesValues.indexOf(el.user.name) == -1) {
-    uniquesValues.push(el.user.name)
-  }
-})
+  initialArray.forEach((el) => {
+    if (uniquesValues.indexOf(el.user.name) == -1) {
+      uniquesValues.push(el.user.name)
+    }
+  })
 
-//2.Сделать шаблон финального обьекта для каждого сотрудника
+  //2.Сделать шаблон финального обьекта для каждого сотрудника
 
-let draftObject = {}
+  const draftObject = {}
 
-uniquesValues.forEach((el) => {
-  draftObject[el] = {
-    userId: '',
-    name: '',
-    weekendDates: [],
-  }
-})
+  uniquesValues.forEach((el) => {
+    draftObject[el] = {
+      userId: '',
+      name: '',
+      weekendDates: [],
+    }
+  })
 
-/*3.Пройтись но всему изначальному масиву отпусков(vacationArray) 
+  /*3.Пройтись но всему изначальному масиву отпусков(initialArray) 
 и по уникальному значению имени сотрудника добавить в draftObject значения отпусков в массив weekendDates
 */
-vacationArray.forEach((el) => {
-  if (draftObject[el.user.name].userId === '') {
-    draftObject[el.user.name].userId = el.user._id
-    draftObject[el.user.name].name = el.user.name
-    draftObject[el.user.name].weekendDates.push({
-      startDate: el.startDate,
-      endDate: el.endDate,
-    })
-  } else {
-    draftObject[el.user.name].weekendDates.push({
-      startDate: el.startDate,
-      endDate: el.endDate,
-    })
+  initialArray.forEach((el) => {
+    if (draftObject[el.user.name].userId === '') {
+      draftObject[el.user.name].userId = el.user._id
+      draftObject[el.user.name].name = el.user.name
+      draftObject[el.user.name].weekendDates.push({
+        startDate: el.startDate,
+        endDate: el.endDate,
+      })
+    } else {
+      draftObject[el.user.name].weekendDates.push({
+        startDate: el.startDate,
+        endDate: el.endDate,
+      })
+    }
+  })
+
+  //4.превратить в масив обьектов
+  const finalArray = []
+
+  for (const [key, value] of Object.entries(draftObject)) {
+    finalArray.push(value)
   }
-})
 
-//4.превратить в масив обьектов
-
-let finalArray = []
-for (const [key, value] of Object.entries(draftObject)) {
-  finalArray.push(value)
+  return finalArray
 }
 
-console.log(finalArray[5])
+console.log(transform(vacationArray))
